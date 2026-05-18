e69de9dec1ad6b7c73fc08b7477545d25f26fbfe
Jordan Loland <jloland@uvic.ca>
2026-05-18 12:44:38 -0700

    All images should be fixed now

--- script.js
+++ script.js
@@ -169,7 +169,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <p>Today I worked on the power harness for the drone. The power harness is the system of wires and connectors that distribute power from the battery to all the different electrical 
             components of the drone. I learned about different wire gauges and how they handle current, and the system of connectors used to distribute power effectively and safely.
             <figure>
-                <img src="pictures/Power Harness.jpg", width="80%", alt="content failed to load">
+                <img src="Pictures/Power Harness.jpg", width="80%", alt="content failed to load">
                 <figcaption>Small part of harness to split power to different components</figcaption>
             </figure>
 
@@ -179,7 +179,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <p>Today it was more retrofitting old GPS systems. This time less soldering and more crimping. I learned what it takes for a good crimp connection, and manually crimped pins onto each of 
             the GPS's output wires. Only one GPS system needed to be updated this week.</p>
             <figure>
-                <img src="pictures/Crimping.jpg", width="80%", alt="content failed to load">
+                <img src="Pictures/Crimping.jpg", width="80%", alt="content failed to load">
                 <figcaption>8 pin GPS with headers crimped on</figcaption>
             </figure>
 
